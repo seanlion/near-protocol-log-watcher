ed3687baea236b0bb8a2326509a13241ffa9a4e8
seanlion <skyhs3507@likelion.org>
2022-09-05 23:01:46 +0900

    Add filtering

--- parser.js
+++ parser.js
@@ -3,10 +3,17 @@ Tail = require('tail').Tail;
 var options= {separator: /[\r]{0,1}\n/,  fsWatchOptions: {}, fromBeginning: false, nLines:1, follow: true}
 tail = new Tail("./neardmain.log", options);
 
-tail.on("line", function(data) {
-    console.log(data);
+const filterType = ['ERROR', 'panic', 'DEBUG']
+tail.on("line", function(passedLine) {
+    const errorFound = filterType.filter((str) => str.toLowerCase().includes(passedLine.toLowerCase()));
+    if (errorFound){
+        console.log("Error Found!");
+    }
+    else{
+        console.log(passedLine);
+    }
   });
   
 tail.on("error", function(error) {
-console.log('ERROR: ', error);
+    console.log('Tail file error: ', error);
 });
